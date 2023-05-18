12d86e9237e5ff8330517aa27b03597c2db0147b
RealHariKrishna <harikrishnamachavaram90@gmail.com>
2023-05-18 12:18:09 +0000

    express js get method

---
+++ GetbooksAPI.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+const app = express();
+
+const dbPath = path.join(__dirname, "goodreads.db");
+let db = null;
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3002, () => {
+      console.log("server Running at http://localhost:3002/");
+    });
+  } catch (e) {
+    console.log(`DB Error: ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `select * from book order by book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
